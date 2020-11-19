652f28433ba4f177941b3a9188b610714f823a29
Shindhi <diogoshindhinaka@gmail.com>
2020-11-19 20:44:45 -0300

    feat: Criação da página de listagem de amigos.

--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-import { Text } from 'react-native';
+import User1 from '../../assets/user1.png';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderText,
+  ListFriends,
+  NewChatButton,
+  FriendImage,
+  Friend,
+  FriendName,
+} from './styles';
 
 const Friends: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <Text>Friends</Text>
+      <Header>
+        <HeaderText>Amigos</HeaderText>
+      </Header>
+
+      {/* Listagem de amigos */}
+      <ListFriends>
+        <NewChatButton onPress={() => navigation.navigate('PrivateChat')}>
+          <FriendImage source={User1} />
+
+          <Friend>
+            <FriendName>User X</FriendName>
+          </Friend>
+        </NewChatButton>
+
+        <NewChatButton onPress={() => navigation.navigate('PrivateChat')}>
+          <FriendImage source={User1} />
+
+          <Friend>
+            <FriendName>User X</FriendName>
+          </Friend>
+        </NewChatButton>
+
+        <NewChatButton onPress={() => navigation.navigate('PrivateChat')}>
+          <FriendImage source={User1} />
+
+          <Friend>
+            <FriendName>User X</FriendName>
+          </Friend>
+        </NewChatButton>
+      </ListFriends>
     </Container>
   );
 };
--- src/pages/Friends/styles.ts
+++ src/pages/Friends/styles.ts
@@ -1,5 +1,56 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  padding-top: 10px;
 
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  justify-content: center;
+  align-items: center;
+  border-bottom-color: #fff;
+  border-bottom-width: 1px;
+  padding-bottom: 10px;
+`;
+
+export const HeaderText = styled.Text`
+  color: #fff;
+  font-family: roboto_700;
+  font-size: 26px;
+`;
+
+export const ListFriends = styled.ScrollView`
+  padding: 0 20px;
+  margin-top: 15px;
+
+  flex: 1;
+`;
+
+export const NewChatButton = styled.TouchableOpacity`
+  padding: 5px 0;
+  margin-bottom: 15px;
+
+  flex-direction: row;
+`;
+
+export const FriendImage = styled.Image`
+  width: 60px;
+  height: 60px;
+
+  border-radius: 4px;
+`;
+
+export const Friend = styled.View`
+  margin-left: 20px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FriendName = styled.Text`
+  color: #fff;
+  font-family: roboto_500;
+  font-size: 16px;
 `;
