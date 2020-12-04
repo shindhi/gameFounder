294bcdc0f95aad03e51451264ca00af3365a698e
Shindhi <diogoshindhinaka@gmail.com>
2020-12-03 23:03:27 -0300

    Implementado a tela de pesquisa de grupo

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+
+import Lol from '../../assets/lol.png';
+
+import {
+  Container,
+  ListGroups,
+  Group,
+  ImageGroup,
+  InfoGroup,
+  NameGroup,
+  ContainerSubInfo,
+  Wave,
+} from './styles';
 
 const Search: React.FC = () => {
+  const navigation = useNavigation();
+
+  const filter = useCallback(() => {}, []);
+
   return (
     <Container>
-      <Text>Page Search</Text>
+      <Input icon="search" name="searchGroup" />
+
+      <ListGroups>
+        <Group
+          onPress={() => {
+            navigation.navigate('PrivateChat');
+          }}
+        >
+          <ImageGroup source={Lol} />
+
+          <InfoGroup>
+            <NameGroup>Amoung US</NameGroup>
+
+            <ContainerSubInfo>
+              <Wave>2/10</Wave>
+
+              <MaterialCommunityIcons name="lock" size={20} color="#7D7D7F" />
+            </ContainerSubInfo>
+          </InfoGroup>
+        </Group>
+
+        <Group>
+          <ImageGroup source={Lol} />
+
+          <InfoGroup>
+            <NameGroup>Amoung US</NameGroup>
+
+            <ContainerSubInfo>
+              <Wave>2/10</Wave>
+
+              <MaterialCommunityIcons name="lock" size={20} color="#7D7D7F" />
+            </ContainerSubInfo>
+          </InfoGroup>
+        </Group>
+      </ListGroups>
     </Container>
   );
 };
--- src/pages/Search/styles.ts
+++ src/pages/Search/styles.ts
@@ -1,5 +1,50 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  background: #191a1e;
+  padding: 10px 20px 0;
+
   flex: 1;
+`;
+
+export const ListGroups = styled.ScrollView`
+  margin-top: 15px;
+
+  flex: 1;
+`;
+
+export const Group = styled.TouchableOpacity`
+  margin-bottom: 20px;
+
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const ImageGroup = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 4px;
+  margin-right: 15px;
+`;
+
+export const InfoGroup = styled.View``;
+
+export const NameGroup = styled.Text`
+  color: #fff;
+  font-family: roboto_400;
+`;
+
+export const ContainerSubInfo = styled.View`
+  padding-top: 8px;
+
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Wave = styled.Text`
+  color: #7d7d7f;
+  font-size: 12px;
+  font-family: roboto_400;
+
+  margin-right: 8px;
 `;
